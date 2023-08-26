d075b6f7990fa47b5d22b34b55d9c1da127867d3
Oguzhnblt <oguzhanbolat0@gmail.com>
2023-08-26 17:37:27 +0300

    Editing CardView

---
+++ Memorize/CardView.swift
@@ -0,0 +1,40 @@
+//
+//  CardView.swift
+//  Memorize
+//
+//  Created by Oğuz on 26.08.2023.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    
+    let card: MemoryGame<String>.Card
+    
+    init(_ card: MemoryGame<String>.Card) {
+        self.card = card
+    }
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(card.content)
+                    .font(.system(size: 200))
+                    .minimumScaleFactor(0.01)
+                    .aspectRatio(1, contentMode: .fit)
+            }
+            .opacity(card.isFaceUp ? 1 : 0)
+            base.fill()
+                .opacity(card.isFaceUp ? 0 : 1)
+        }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+    }
+}
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView(MemoryGame<String>.Card(id: "test", content: "x"))
+    }
+}
--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -26,48 +26,28 @@ struct EmojiMemoryGameView: View {
     
     // Ekranda görünecek kartların düzen View'i
     private var cards: some View {
-        AspectVGrid(items: viewModel.cards, aspectRatio: aspectRatio) {card in
-            CardView(card)
-                .padding(4)
-                .onTapGesture {
-                    viewModel.choose(card)
-                }
-        }
-        .foregroundColor(Color.orange)
-    }
-}
-   
-    struct CardView: View {
-        
-        let card: MemoryGame<String>.Card
-        
-        init(_ card: MemoryGame<String>.Card) {
-            self.card = card
-        }
-        
-        var body: some View {
-            ZStack {
-                let base = RoundedRectangle(cornerRadius: 12)
-                Group {
-                    base.fill(.white)
-                    base.strokeBorder(lineWidth: 2)
-                    Text(card.content)
-                        .font(.system(size: 200))
-                        .minimumScaleFactor(0.01)
-                        .aspectRatio(1, contentMode: .fit)
-                    
-                }
-                base.fill().opacity(card.isFaceUp ? 0 : 1)
-                  
+        LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 10), count: 6), spacing: 10) {
+            ForEach(viewModel.cards) { card in
+                CardView(card)
+                    .aspectRatio(2/5, contentMode: .fit)
+                    .onTapGesture {
+                        viewModel.choose(card)
+                    }
+                    .foregroundColor(.orange)
             }
         }
+        .padding(10)
     }
-    
-    
-    
-    struct EmojiMemoryGameView_Previews: PreviewProvider {
-        static var previews: some View {
-            EmojiMemoryGameView()
-        }
+}
+
+
+
+
+
+struct EmojiMemoryGameView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiMemoryGameView()
     }
+}
+
 
